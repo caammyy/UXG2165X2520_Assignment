7b8bfd45a72a4febb16afdedd3300c20a4a8a399
caammyy <cammylimzy@gmail.com>
2023-07-11 04:20:51 +0800

    merge with a sad attempt at dialogue

---
+++ Assets/Script/Data/RefCharacter.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class RefCharacter
+{
+    public string characterID;
+    public string characterName;
+    public int characterHp;
+    public string weaponID;
+}
---
+++ Assets/Script/DataManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class DataManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoadRefData();
+    }
+
+    public void LoadRefData()
+    {
+        string filePath = Path.Combine(Application.dataPath, "Script/Data/charactersData.txt");
+        //persistent data path is for when you want to save the game data(?), data path is for the data alr inside unity
+
+        string dataString = File.ReadAllText(filePath);
+        Debug.Log(dataString);
+
+        DataScript datascript = JsonUtility.FromJson<DataScript>(dataString);
+    }
+}
